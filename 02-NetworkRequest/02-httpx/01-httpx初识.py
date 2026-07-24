a5fa2f919af62446062ec2178802a76e00fb33c8
macong <macong0419email@163.com>
2026-07-24 18:15:08 +0800

    初识httpx

--- 02-NetworkRequest/02-httpx/01-httpx初识.py
+++ 02-NetworkRequest/02-httpx/01-httpx初识.py
@@ -27,3 +27,11 @@ params = {
 }
 response = httpx.get("https://httpbin.org/get", params=params)
 print(response.text)
+
+# 几种参数类型
+# params / data / json
+"""
+    params: 位置在URL之后，没有content-type，一般在 GET 方法中使用，主要做 查询 操作
+    data:   位置在body(请求体)中，content-type为表单形式，一般在 POST 方法中使用，主要是 提交表单 操作
+    json:   位置在body(请求体)中，content-type为json形式，一般在 POST、PUT 方法中使用，主要是API数据交互
+"""
