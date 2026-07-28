3ea86e5aa416842c380a142f68bcb1d7ac5864c3
macong <macong0419email@163.com>
2026-07-29 00:39:47 +0800

    update：补充查询功能

--- 03-FastAPI/fastapi-project/routers/user.py
+++ 03-FastAPI/fastapi-project/routers/user.py
@@ -5,9 +5,10 @@
    :date: 2026年07月28日,18:11:21
  """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schemas.user import (UserCreate, UserResponse)
 from services import user_service
+
 
 # 创建一个路由
 router = APIRouter(
@@ -15,15 +16,25 @@ router = APIRouter(
     tags=["users"]
 )
 
-@router.get("list")
+@router.get("/list")
 async def get_users():
     return user_service.users
 
 
 # 用户注册
 @router.post(
-    "register",
-    response_model=UserResponse
+    "/register",
+    response_model=UserResponse,
+    status_code=201
 )
 async def register(user: UserCreate):
     return user_service.register_user(user)
+
+
+# 查询用户信息
+@router.get(
+    "/{user_id}",
+    response_model=UserResponse
+)
+async def get_user(user_id: int):
+    return user_service.get_user_byid(user_id)
