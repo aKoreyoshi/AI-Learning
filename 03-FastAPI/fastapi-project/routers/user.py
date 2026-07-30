bed3844bca26b3ce4c787c727c093d745c8f7c8a
macong <macong0419email@163.com>
2026-07-31 00:31:25 +0800

    update：更新补充全局异常处理

--- 03-FastAPI/fastapi-project/routers/user.py
+++ 03-FastAPI/fastapi-project/routers/user.py
@@ -5,9 +5,11 @@
    :date: 2026年07月28日,18:11:21
  """
 
-from fastapi import APIRouter, HTTPException
-from schemas.user import (UserCreate, UserResponse)
+from fastapi import APIRouter, HTTPException, Depends
+from schemas.user import UserCreate, UserResponse, UserLogin
 from services import user_service
+from dependencies import get_current_user
+from schemas.common import ResultModel
 
 
 # 创建一个路由
@@ -24,17 +26,34 @@ async def get_users():
 # 用户注册
 @router.post(
     "/register",
-    response_model=UserResponse,
-    status_code=201
+    response_model=ResultModel,
 )
 async def register(user: UserCreate):
-    return user_service.register_user(user)
+
+    result = user_service.register_user(user)
+    return ResultModel(data=result)
+
+# 用户登录
+@router.post("/login")
+async def login(user: UserLogin):
+    result = user_service.login_user(user)
+    return {"message": "登录成功"}
+
+
+# 获取当前用户信息
+@router.get("/profile", response_model=UserResponse)
+async def profile(
+        user = Depends(get_current_user)
+    ):
+    return user
 
 
 # 查询用户信息
 @router.get(
     "/{user_id}",
-    response_model=UserResponse
+    response_model=ResultModel
 )
 async def get_user(user_id: int):
-    return user_service.get_user_byid(user_id)
+
+    result = user_service.get_user_byid(user_id)
+    return ResultModel(data=result)
