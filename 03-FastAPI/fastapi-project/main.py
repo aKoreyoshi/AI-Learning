509db79a7a99b11fd143bd3ff9a45862f6446459
macong <macong0419email@163.com>
2026-07-31 00:35:18 +0800

    update：更新补充全局异常处理

--- 03-FastAPI/fastapi-project/main.py
+++ 03-FastAPI/fastapi-project/main.py
@@ -6,12 +6,30 @@
  """
 
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
 from routers import user
+from exceptions.business_exception import BusinessException
 
 app = FastAPI(
     title="User Management API",
     version="1.0.0"
 )
+
+# 添加全局异常处理
+@app.exception_handler(BusinessException)
+async def business_exception_handler(
+        request,
+        exc
+    ):
+    return JSONResponse(
+        status_code=400,
+        content={
+            "code": exc.code,
+            "message": exc.message,
+            "data": None
+        }
+    )
 
 # 将路由注册到app中
 app.include_router(
