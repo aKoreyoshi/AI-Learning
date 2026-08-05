b016fa346657f021225343ab9831571e25e84511
macong <macong0419email@163.com>
2026-08-05 21:10:13 +0800

    add：测试实例，添加用户表

---
+++ 03-FastAPI/fastapi_project/crud/test_crud.py
@@ -0,0 +1,18 @@
+"""
+   :author: Kairos
+   :description: 测试连接数据库操作
+   :version: 1.0
+   :date: 2026年08月05日,16:19:15
+ """
+
+from database.database import SessionLocal
+from crud.user import create_user
+
+db = SessionLocal()
+
+user = create_user(db, "马聪", "123")
+
+print(user.id)
+print(user.username)
+
+db.close()
