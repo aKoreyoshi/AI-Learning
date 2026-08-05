46dc6c4e3477f6efe575199260611cca2032aed4
macong <macong0419email@163.com>
2026-08-05 21:15:33 +0800

    update：加入真实认证，通过jwt解析token获取当前用户信息

--- 03-FastAPI/fastapi_project/dependencies/auth.py
+++ 03-FastAPI/fastapi_project/dependencies/auth.py
@@ -5,9 +5,16 @@
    :date: 2026年07月30日,16:59:26
  """
 from fastapi import Header, HTTPException, Depends
-import security
-from services import user_service
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
+from database.database import get_db
+from utils.jwt import decode_access_token
+from crud.user import get_user_by_username
+from exceptions.auth_exception import TokenInvalidException, TokenDataErrorException
+from exceptions.user_exception import UserNotFoundException
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 # 获取当前用户token
 def get_token(
@@ -18,20 +25,26 @@ def get_token(
 
 # 获取当前用户
 def get_current_user(
-        token = Depends(get_token)
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_db)
     ):
-    payload = security.decode_token(token)
+    print(f"========================token = {token}")
+    # 解析token
+    pyload = decode_access_token(token)
+    print(f"========================pyload = {pyload}")
+    # token无效
+    if not pyload:
+        raise TokenInvalidException()
 
-    if payload is None:
-        raise HTTPException(
-            status_code=401,
-            detail="无效的token"
-        )
-    user = user_service.get_user_byid(payload["user_id"])
+    # 获取到用户名
+    username = pyload.get("username")
+    if not username:
+        raise TokenDataErrorException()
 
-    if user is None:
-        raise HTTPException(
-            status_code=401,
-            detail="用户不存在"
-        )
+    # 根据用户名拿到用户信息
+    user = get_user_by_username(db, username)
+
+    if not user:
+        raise UserNotFoundException()
+    # 返回用户信息
     return user
